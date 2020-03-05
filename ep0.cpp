0be29ec6544b1909a2a09c3a30f0d1a002e46133
Gabriel Henrique Pinheiro Rodrigues <gabrielhpr@hotmail.com.br>
2020-03-04 22:13:23 -0300

    Implementacao dos dados do aeroporto e transicao

--- ep0.cpp
+++ ep0.cpp
@@ -4,21 +4,13 @@
 
 using namespace std;
 
-int main() {
-    int T = 10; /*Tempo de simulação*/
-    /*K - Número máximo de aviões que se comunicam com a torre por unid de tempo, 
-    C - número máximo de tempo de combustível, V - Tempo máximo estimado de voo */
-    int K = 15, C, V;
-    long long i;
-    int time=8*60; /*Horário*/
-    int j; /*Iterador*/
-
+Aviao* geradorDeAvioes() {
     /********TESTES*******/
     /*O nosso aeroporto é o de CONGONHAS*/
+    
 
-    /*SEMENTE*/
-    int semente = 23;
-    srand(semente);
+    /*Número de 3 dígitos*/
+    int numeroDoAviao;
 
     /*As companhias aéreas são AZUL, GOL, LATAM, AVIANCA, AIR FRANCE, EMIRATES
     COPA AIRLINES*/
@@ -35,32 +27,72 @@ int main() {
     "SJP", "SOD", "VOT", "BAT", "BAU", "QCP", "DNO", "GRP", "NTM", "PNB", "IDO",
      "CCI", "BNU", "FEJ"};
 
-    /*Número de 3 dígitos*/
-    int numeroDoAviao;
+    return new Aviao(companhiasAereas[rand()%7], rand()%900 + 100, false, rand()%2, aeroportos[rand()%33], rand()%2, 40, 0);
+}
 
-    /*********************/
+
+
+int main() {
+    int T = 10; /*Tempo de simulação*/
+    /*K - Número máximo de aviões que se comunicam com a torre por unid de tempo, 
+    C - número máximo de tempo de combustível, V - Tempo máximo estimado de voo */
+    int K = 15, C, V;
+    long long i;
+    int time=8*60; /*Horário*/
+    int j, k; /*Iterador*/
+
+    /*SEMENTE*/
+    int semente = 23;
+    srand(semente);
+
     Aeroporto aeroporto;
 
     Aviao* aviao;    
     /*Delay na exibição*/
-    for (i = 0; i < 1/*T*/; i++) {
+    for (i = 0; i < 4/*T*/; i++) {
         //if(i % SECOND == 0) {
-            cout << "**********AEROPORTO DE CONGONHAS**********" << endl << endl;
-            printf("--------------HORÁRIO %02d:%02d--------------\n\n", time/60, time%60); 
+            cout << "*****************************AEROPORTO DE CONGONHAS*****************************" << endl << endl;
+            printf("----------------------------------HORÁRIO %02d:%02d---------------------------------\n\n", time/60, time%60); 
             time++;
 
-            for (j = 0; j < 10/*K*/; j++) {
+            for (j = 0; j < 3/*K*/; j++) {
                 /*Objeto avião*/
-                aviao = new Aviao(companhiasAereas[rand()%7], rand()%900 + 100, false, rand()%2, aeroportos[rand()%33], rand()%2, 40, 0);
-                cout << "Novo aviao: ";
-                aeroporto.printaElemento(aviao);
+                aviao = geradorDeAvioes();
+                //cout << "Novo aviao: ";
+                //aeroporto.printaElemento(aviao);
                 
+                /*Insere os novos avioes na fila*/
                 aeroporto.contatoComATorre(aviao);
-                
-                aeroporto.printaFila();
+
+                /*OUTPUT*/
+                /*Quais avioes estao esperando para decolar e pousar*/
+                //aeroporto.printaFila(0, 0);
             }
-            
-            cout << "******************************************" << endl << endl;
+            aeroporto.atualizaFilaDeVoos();
+            aeroporto.liberaVoos();
+            aeroporto.atualizaDispDePista();
+            aeroporto.coletaEstatisticasEPrinta();
+
+
+            cout << "********************************************************************************" << endl << endl;
+            int num = 27;
+            bool parou = false;
+            for(j = 0; j < num && !parou; j++) {
+                for ( k = 0; k < num; k++) {
+                    if(k == num/2 && j == (num/2) )
+                        cout << "   ";
+                    else if ( k <= j && j < (num/2)+1 )
+                        cout << "   ";
+                    else if( k >= num-j-1 && j < (num/2)+1 )
+                        cout << "   ";
+                    else if(j < (num/2) ) 
+                        cout << " * ";
+                    else
+                        parou = true;
+                }
+                cout << endl;
+            }
+                
         //}
     }
     
--- ep0.h
+++ ep0.h
@@ -44,14 +44,27 @@ class Aeroporto {
         bool pista1Disponivel;
         bool pista2Disponivel;
         bool pista3Disponivel;
+        bool todasPistasOcupadas;
 
         int tempLibPista1;
         int tempLibPista2;
         int tempLibPista3;
 
+        /*Dados estatísticos*/
+        int nAvioesQuePousaram;
+        int nAvioesAPousar;
+        int tMedioEsperaPouso;
+        int tMedioEsperaDecolagem;
+        int quantMediaCombAvAPousar;
+        int quantMediaCombAvPousaram;
+        int quantVoosEmerg;
+
+        /*Insere os aviões na fila*/
         void contatoComATorre(Aviao* aviao);
+        /*Informa se o avião consegue utilizar alguma pista ou não*/
         bool solicDePousoDec(Aviao* aviao);
         void atualizaDispDePista();
+        bool haPistaLivre();
 
         /*Controle das filas*/
         int numElem;
@@ -61,21 +74,37 @@ class Aeroporto {
         /*Métodos*/
         void insereNoFim(Aviao* aviao);
         void insereNoInicio(Aviao* aviao); 
-    public:
         Aviao* remove(elementoDaFila* elm);
-        void checa();
+    public:
+        /*Rearranja os elementos da fila de acordo com suas prioridades*/
+        void atualizaFilaDeVoos();
+        /*Libera os voos de acordo com a disponibilidade das pistas*/
+        void liberaVoos();
+        void coletaEstatisticasEPrinta();
+        void printaAviao(Aviao* aviao, int pouso);
         void printaElemento(Aviao* aviao);
-        void printaFila();
+        /*tipoDePrintagem(0-exibe elemento completo, 1-exibe o ID do aviao se atender aos parametros - pouso ou não)*/
+        void printaFila(int tipoDePrintagem, int pouso);
+
 };
 
 Aeroporto::Aeroporto() {
     pista1Disponivel = true;
     pista2Disponivel = true;
     pista3Disponivel = true;
+    todasPistasOcupadas = false;
 
     tempLibPista1 = 0;
     tempLibPista2 = 0;
     tempLibPista3 = 0;
+
+    nAvioesQuePousaram = 0;
+    nAvioesAPousar = 0;
+    tMedioEsperaPouso = 0;
+    tMedioEsperaDecolagem = 0;
+    quantMediaCombAvAPousar = 0;
+    quantMediaCombAvPousaram = 0;
+    quantVoosEmerg = 0;
 
     numElem = 0;
     /*Inicializa a cabeça da fila */
@@ -98,20 +127,37 @@ Aeroporto::~Aeroporto() {
     /*Deleta a cabeça*/
     delete cab;
 }
-void Aeroporto :: printaElemento(Aviao* aviao) {
-    cout << "N: " << aviao->numeroDoAviao << " comp: " << aviao->companhiaArea <<
-    " emerg: " << aviao->emergencia << " quantCombus: " << aviao->quantCombust <<
-    " durVoo: " << aviao->duracaoDoVoo << endl;
+void Aeroporto :: printaAviao(Aviao* aviao, int pouso) {
+    if(pouso && aviao->pouso)
+        cout << aviao->companhiaArea << aviao->numeroDoAviao << "; ";
+    else if(!pouso && !aviao->pouso)
+        cout << aviao->companhiaArea << aviao->numeroDoAviao << "; ";
 }
-void Aeroporto :: printaFila() {
+void Aeroporto :: printaElemento(Aviao* aviao) {
+    cout << aviao->companhiaArea << aviao->numeroDoAviao <<
+    " emerg: " << aviao->emergencia << " , quantCombus: " << aviao->quantCombust <<
+    " , durVoo: " << aviao->duracaoDoVoo << " , tempDeEspera: " << aviao->tempoDeEspera <<
+    " , destino: "<< aviao->aeroportoDestOrig << endl;
+}
+void Aeroporto :: printaFila(int tipoDePrintagem, int pouso) {
+    int nPouso, nDec;
+    nPouso = nDec = 0;
     elementoDaFila* aux = cab->atras;
     while (aux != nullptr) {
-        printaElemento(aux->aviao);
+        if(tipoDePrintagem == 0)
+            printaElemento(aux->aviao);
+        else {
+            /*Quebra a linha na printagem do terminal*/
+            if(pouso && aux->aviao->pouso) nPouso++;
+            else if(!pouso && !aux->aviao->pouso) nDec++;
+            if(nPouso == 5 || nDec == 5) cout << endl << "   ";
+
+            printaAviao(aux->aviao, pouso);
+        }
         aux = aux->atras;
     }
-    cout << endl << endl;
+    cout << endl;
 }
-
 /*Voos sem prioridade são colocados no final da fila*/
 void Aeroporto :: insereNoFim(Aviao* aviao) {
     elementoDaFila* aux = new elementoDaFila;
@@ -157,32 +203,120 @@ Aviao* Aeroporto :: remove(elementoDaFila* elm) {
 void Aeroporto :: contatoComATorre(Aviao* aviao) {
     if(aviao->emergencia || aviao->quantCombust == 0) {
         /*Coloca na fila*/
-        cout << "Colocar no inicio" << endl;
+        //cout << "Colocar no inicio" << endl;
         insereNoInicio(aviao);
     }
     else {
-        cout << "Colocar no fim" << endl;
+        //cout << "Colocar no fim" << endl;
         insereNoFim(aviao);
     }
 }
 bool Aeroporto :: solicDePousoDec(Aviao* aviao) {
     /*Pode pousar/decolar em qualquer pista*/
         if(pista3Disponivel && (aviao->emergencia || aviao->pouso == false) ) {
+            /*Aumenta o número de voos de emergência realizados*/
+            if(aviao->emergencia) quantVoosEmerg++;
+            if(aviao->pouso) {
+                quantMediaCombAvPousaram += aviao->quantCombust;
+                nAvioesQuePousaram++;
+            }
             pista3Disponivel = false;
             tempLibPista3 = 2;
             return true;
         }
         else if(pista2Disponivel) {
+            if(aviao->pouso) {
+                quantMediaCombAvPousaram += aviao->quantCombust;
+                nAvioesQuePousaram++;
+            }
             pista2Disponivel = false;
             tempLibPista2 = 2;
             return true;
         }
         else if(pista1Disponivel) {
+            if(aviao->pouso) {
+                quantMediaCombAvPousaram += aviao->quantCombust;
+                nAvioesQuePousaram++;
+            }
             pista1Disponivel = false;
             tempLibPista1 = 2;
             return true;
         }
         else return false;
+}
+void Aeroporto :: atualizaDispDePista() {
+    if(!pista1Disponivel) 
+        if(--tempLibPista1 == 0) pista1Disponivel = true;
+    if(!pista2Disponivel) 
+        if(--tempLibPista2 == 0) pista2Disponivel = true;
+    if(!pista3Disponivel)
+        if(--tempLibPista3 == 0) pista3Disponivel = true;
+}
+bool Aeroporto :: haPistaLivre() {
+    return (pista1Disponivel || pista2Disponivel || pista3Disponivel);
+}
+void Aeroporto :: atualizaFilaDeVoos() {
+
+}
+void Aeroporto :: liberaVoos() {
+    elementoDaFila* aux = cab->atras;
+    elementoDaFila* troca;
+    while(aux != nullptr && haPistaLivre() == true) {
+        if(solicDePousoDec(aux->aviao) == true) {
+            troca = aux->atras;
+            remove(aux);
+            aux = troca;
+        }
+        else {
+            aux = aux->atras;
+        }
+    }
+    //printaFila();   
+}
+void Aeroporto :: coletaEstatisticasEPrinta() {
+    elementoDaFila* aux = cab->atras;
+
+    tMedioEsperaPouso = 0;
+    tMedioEsperaDecolagem = 0;
+    quantMediaCombAvAPousar = 0;
+    nAvioesAPousar = 0;
+
+    while(aux != nullptr) {
+        if(aux->aviao->pouso) {
+            tMedioEsperaPouso += aux->aviao->tempoDeEspera;
+            quantMediaCombAvAPousar += aux->aviao->quantCombust;
+            nAvioesAPousar++;
+        }
+        else if(aux->aviao->pouso == 0)
+            tMedioEsperaDecolagem += aux->aviao->tempoDeEspera;
+        aux = aux->atras;
+    }
+
+    if(nAvioesAPousar != 0) {
+        tMedioEsperaPouso /= nAvioesAPousar;
+        quantMediaCombAvAPousar /= nAvioesAPousar;
+    }
+    /*Não há informações acerca dos aviões*/
+    else {
+        tMedioEsperaPouso = -1;
+        quantMediaCombAvAPousar = -1;
+    }
+    ((numElem-nAvioesAPousar) != 0) ? tMedioEsperaDecolagem /= (numElem - nAvioesAPousar) : tMedioEsperaDecolagem = -1;
+    ((nAvioesQuePousaram != 0)) ? quantMediaCombAvPousaram /= nAvioesAPousar : quantMediaCombAvPousaram = -1;
+
+    cout << "-----------------------------ESTATÍSTICAS DO AEROPORTO--------------------------" << endl << endl;
+    cout << " * Os aviões que estão esperando para pousar são: ";
+    printaFila(1, 1);
+    cout << endl;
+    cout << " * Os aviões que estão esperando para decolar são: ";
+    printaFila(1, 0);
+    cout << endl;
+    cout << " * O tempo médio de espera para pouso é: " << tMedioEsperaPouso << endl << endl;
+    cout << " * O tempo médio de espera para decolagem é: " << tMedioEsperaDecolagem << endl << endl;
+    cout << " * A quantidade média de combustível dos aviões esperando para pousar é: " << quantMediaCombAvAPousar << endl << endl;
+    cout << " * A quantidade média de combustível dos aviões que pousaram: " << quantMediaCombAvPousaram << endl << endl;
+    cout << " * A quantidade de aviões pousando/decolando em condições de emergência é: " << quantVoosEmerg << endl << endl;
+    cout << "--------------------------------------------------------------------------------" << endl;
 }
 
 #endif 
